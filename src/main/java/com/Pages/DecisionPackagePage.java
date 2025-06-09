b3edb3c3787f1ea5ce15c4589e038f2a7e54b27e
Pankaj Ghosh <pghosh@index-analytics.com>
2025-06-09 14:46:26 -0400

    Completed all three type of submision

--- src/main/java/com/Pages/DecisionPackagePage.java
+++ src/main/java/com/Pages/DecisionPackagePage.java
@@ -7,6 +7,7 @@ import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 
 import com.Locators.DecisionPackage_Loc;
 import com.utility.SafeActions;
@@ -14,8 +15,9 @@ import com.utility.SafeActions;
 public class DecisionPackagePage extends SafeActions implements DecisionPackage_Loc {
 
 	public void validateMandatoryFieldsErrorMessages() {
+
 		jsClickOn(newDecisionPackage, "newDecisionPackage");
-		
+
 		typeText(submitDate, fetchWeekdayDate("MMM dd, yyyy", 2), "submitDate");
 		typeText(acknowledgementDate, fetchWeekdayDate("MMM dd, yyyy", 2), "acknowledgementDate");
 		jsClickOn(submitButton, "submitButton");
@@ -25,6 +27,8 @@ public class DecisionPackagePage extends SafeActions implements DecisionPackage_
 		verifyTextDisplay(At_least_one_Funding_Type_must_be_selected, "At least one Funding Type must be selected.");
 		verifyTextDisplay(Submission_Date_is_required, "Submission Date must be today or earlier.");
 		takeScreenshotFor("First half of validation form");
+		jsClickOn(Emergency_Request, "Emergency_Request");
+		waitFor(2);
 		jsClickOn(Emergency_Request, "Emergency_Request");
 		waitFor(2);
 		scrollToBottomOfthePage();
@@ -38,15 +42,18 @@ public class DecisionPackagePage extends SafeActions implements DecisionPackage_
 
 	Robot rb;
 
-	public void verifyUserCanCreate_DecisionPackage() {
+	public void verifyUserCanCreate_DecisionPackage(By submisionTypeLoc, String text) {
 		scrollToTopofThePage();
 		String randomChar = Character.toString(getRandomStateInitial());
+		waitFor(2);
 		typeText(State_Medicaid_Agency_Input, randomChar, "State_Medicaid_Agency_Input");
 		jsClickOn(State_Medicaid_Agency_Option, "State_Medicaid_Agency_Option");
 		jsClickOn(Submission_Type, "Submission_Type");
-		jsClickOn(SubmissionTypeOption, "SubmissionTypeOption - APD");
+		jsClickOn(submisionTypeLoc, "SubmissionTypeOption");
 		jsClickOn(fundType_E_E, "fundType_E_E");
-		typeText(estimatedAmount, "1000", "estimatedAmount");
+		if (text.contains("APD")) {
+			typeText(estimatedAmount, "1000", "estimatedAmount");
+		}
 		typeText(submitDate, fetchWeekdayDate("MMM dd, yyyy", -1), "submitDate");
 		typeText(acknowledgementDate, fetchWeekdayDate("MMM dd, yyyy", -1), "acknowledgementDate");
 		jsClickOn(oPsDiv_HHS, "oPsDiv_HHS");
@@ -80,38 +87,47 @@ public class DecisionPackagePage extends SafeActions implements DecisionPackage_
 
 	}
 
-	public void verifyMandatoryFields_OnDecisionPackage() {
-		waitFor(2);
+	public void verifyMandatoryFields_OnDecisionPackage(String title, String submisionType) {
+		waitFor(4);
 		String directorState = getTextFromUI(State_Medicaid_Agency, "State_Medicaid_Agency");
 		System.out.println("directorName: " + directorState);
-		verifyTextDisplay(decisionPackageTitle, "E&E-HHS-APD-Emergency Request");
+		verifyTextDisplay(decisionPackageTitle, title);
 		takeScreenshotFor("After Decision Package created verifying title");
 		jsClickOn(Edit_Recommended_Action, "Edit_Recommended_Action");
+		waitFor(1);
 		jsClickOn(Recommended_Action, "Recommended_Action");
 		waitFor(2);
 		jsClickOn(Approve_Option, "Approve_Option");
-
+		jsClickOn(event_Summary_TextArea, "event_Summary_TextArea");
+		waitFor(2);
+		jsClickOn(event_Summary_TextArea_AfterClick, "event_Summary_TextArea_AfterClick");
+		waitFor(2);
+		typeText(event_Summary_TextArea_AfterClick, "Event Summary Added", "event_Summary_TextArea_");
+		waitFor(5);
 		typeText(State_Medicaid_Director_Input, directorState.split(" ")[0], "State_Medicaid_Director_Input");
 		waitFor(5);
 		String directorName = getAttribute(state_Medicaid_Director_Option, "title", "state_Medicaid_Director_Option");
 		System.out.println("directorName: " + directorName);
 		jsClickOn(state_Medicaid_Director_Option, "state_Medicaid_Director_Option");
 		scrollToBottomOfthePage();
-		jsClickOn(APD_Type, "APD_Type");
-		jsClickOn(APD_Type_Option, "APD_Type_Option");
-		jsClickOn(event_Summary_TextArea, "event_Summary_TextArea");
-		waitFor(2);
-		typeText(event_Summary_TextArea_AfterClick, "Event Summary Added", "event_Summary_TextArea_");
-		waitFor(1);
-		jsClickOn(APD_Update_Type, "APD_Update_Type");
-		jsClickOn(APD_Update_Type_Add_Button, "APD_Update_Type_Add_Button");
-
-		takeScreenshotFor("Star Fields before submit");
+		if (submisionType.contains("APD")) {
+			jsClickOn(APD_Type, "APD_Type");
+			jsClickOn(APD_Type_Option, "APD_Type_Option");
+		}
+		
+		
+		if (submisionType.contains("APD")) {
+			jsClickOn(APD_Update_Type, "APD_Update_Type");
+			jsClickOn(APD_Update_Type_Add_Button, "APD_Update_Type_Add_Button");
+		}
+		takeScreenshotFor("Mandatory Fields before submit");
 		jsClickOn(save_Button, "save_Button");
 		waitFor(3);
 		verifyTextDisplay(approveChoosen, "Approve");
-		verifyTextDisplay(APD_Type_Choosen, "Planning Advanced Planning Document (PAPD)");
-		verifyTextDisplay(APD_Update_Type_Choosen, "Initial Submission");
+		if (submisionType.contains("APD")) {
+			verifyTextDisplay(APD_Type_Choosen, "Planning Advanced Planning Document (PAPD)");
+			verifyTextDisplay(APD_Update_Type_Choosen, "Initial Submission");
+		}
 		verifyTextDisplay(stateMedicaidDirector_selected, directorName);
 		scrollToBottomOfthePage();
 		verifyTextDisplay(event_Summary_Entered, "Event Summary Added");
@@ -121,4 +137,73 @@ public class DecisionPackagePage extends SafeActions implements DecisionPackage_
 		takeScreenshotFor("Second half of the screen with Updated Mandatory fields");
 
 	}
+	
+	public void verifyMandatoryFieldsOn_Contract_DecisionPackage(String title) {
+		waitFor(2);
+		String directorState = getTextFromUI(State_Medicaid_Agency, "State_Medicaid_Agency");
+		System.out.println("directorName: " + directorState);
+		verifyTextDisplay(decisionPackageTitle, title);
+		takeScreenshotFor("After Decision Package created verifying title");
+		jsClickOn(Edit_Recommended_Action, "Edit_Recommended_Action");
+		waitFor(1);
+		jsClickOn(Recommended_Action, "Recommended_Action");
+		waitFor(2);
+		jsClickOn(Approve_Option, "Approve_Option");
+		jsClickOn(event_Summary_TextArea, "event_Summary_TextArea");
+		waitFor(2);
+		jsClickOn(event_Summary_TextArea_AfterClick, "event_Summary_TextArea_AfterClick");
+		waitFor(2);
+		typeText(event_Summary_TextArea_AfterClick, "Event Summary Added", "event_Summary_TextArea_");
+		waitFor(5);
+		typeText(State_Medicaid_Director_Input, directorState.split(" ")[0], "State_Medicaid_Director_Input");
+		waitFor(5);
+		String directorName = getAttribute(state_Medicaid_Director_Option, "title", "state_Medicaid_Director_Option");
+		System.out.println("directorName: " + directorName);
+		jsClickOn(state_Medicaid_Director_Option, "state_Medicaid_Director_Option");
+		scrollToBottomOfthePage();
+		typeText(ContractNumber, "ABC"+fetchDate("yyyy", 4), "ContractNumber");
+		jsClickOn(contractType, "contractType");
+		jsClickOn(contracType_SoleSource, "contracType_SoleSource");
+		typeText(Total_ContractValue, "1000", "Total_ContractValue");
+		typeText(vendor, "test", "Vendor");
+		waitFor(1);
+		jsClickOn(vendorOption, "vendorOption");
+		jsClickOn(claimsProcessing_CheckBox, "claimsProcessing_CheckBox");
+		jsClickOn(auditingCheckBox, "auditingCheckBox");
+		takeScreenshotFor("Mandatory Fields before submit");
+		jsClickOn(save_Button, "save_Button");
+		waitFor(3);
+		verifyTextDisplay(approveChoosen, "Approve");
+		verifyTextDisplay(stateMedicaidDirector_selected, directorName);
+		scrollByPixels(200);
+		verifyTextDisplay(event_Summary_Entered, "Event Summary Added");
+		scrollToTopofThePage();
+		takeScreenshotFor("First half of the screen for contract Type");
+		scrollByPixels(500);
+		takeScreenshotFor("Second half of the screen for contract Type");
+
+	}
+
+	public void verifyDueDate(String screenshotName) {
+		waitFor(3);
+		jsClickOn(decisionPackageTitle, "decisionPackageTitle");
+		scrollToBottomOfthePage();
+		String date1 = getTextFromUI(acknowledgementDate_LabelText, "acknowledgementDate").split(",")[0];
+		String date2 = getTextFromUI(dueDate, "dueDate").split(",")[0];
+		String duedays = getTextFromUI(daysUntilDue, "daysUntilDue");
+		takeScreenshotFor(screenshotName);
+		int duedateFromDates = findTheDifference_Between(date1, date2);
+		int dueDateDisplayed = Integer.parseInt(duedays);
+		Assert.assertEquals(duedateFromDates - 1, dueDateDisplayed,
+				"Due days count NOT displayed as expected so failed");
+	}
+
+	public void selectEmergencyRequestInthe_DecisionPackage() {
+		jsClickOn(edit_Emergency_Request, "edit_Emergency_Request");
+		jsClickOn(emergency_Request_CheckBox, "emergency_Request_CheckBox");
+		jsClickOn(save_Button, "save_Button");
+		waitFor(3);
+		
+	}
+
 }
--- src/test/java/com/Mesh_TestSuites/DecisionPackages.java
+++ src/test/java/com/Mesh_TestSuites/DecisionPackages.java
@@ -11,14 +11,43 @@ public class DecisionPackages extends TestBase {
 	DecisionPackagePage dpp = new DecisionPackagePage();
 
 	@Test(groups = { "Regression"})
-	public void DecisionPackage_TC_001_Verify_OpsTeam_Can_Create_Package() {
+	public void DecisionPackage_TC_001_Verify_DecisionPackage_Creation_with_APD_as_SubmisionType() {
 		// Navigating to Decision package screen
 		homePage.navigateTo_OpsTeam_DecisionPackage_Screen();
 		// Validating the all mandatory fields on the Decision package creation form
 		dpp.validateMandatoryFieldsErrorMessages();
 		// Creation of Decision package
-		dpp.verifyUserCanCreate_DecisionPackage();
+		dpp.verifyUserCanCreate_DecisionPackage(DecisionPackagePage.SubmissionTypeOption_APD, "APD");
+//		dpp.verifyDueDate("Due Date On Emergency");
 		// Validation of required fields after decision package creation 
-		dpp.verifyMandatoryFields_OnDecisionPackage();
+		dpp.verifyMandatoryFields_OnDecisionPackage("E&E-HHS-APD", "APD");
+		dpp.selectEmergencyRequestInthe_DecisionPackage();
+//		dpp.verifyDueDate("Due Date when No Emergency");//
+	}
+	
+	@Test(groups = { "Regression"})
+	public void DecisionPackage_TC_002_Verify_DecisionPackage_Creation_with_RFP_as_SubmisionType() {
+		dpp.jsClickOn(DecisionPackagePage.decisionPackages, "Decision Packages");
+		dpp.jsClickOn(DecisionPackagePage.newDecisionPackage, "newDecisionPackage");
+		// Creation of Decision package
+		dpp.verifyUserCanCreate_DecisionPackage(DecisionPackagePage.SubmissionTypeOption_RFP, "RFP");
+//		dpp.verifyDueDate("Due Date On Emergency");
+		// Validation of required fields after decision package creation 
+		dpp.verifyMandatoryFields_OnDecisionPackage("E&E-HHS-RFP", "RFP");
+		dpp.selectEmergencyRequestInthe_DecisionPackage();
+//		dpp.verifyDueDate("Due Date when No Emergency");
+	}
+	
+	@Test(groups = { "Regression"})
+	public void DecisionPackage_TC_003_Verify_DecisionPackage_Creation_with_Contract_as_SubmisionType() {
+		dpp.jsClickOn(DecisionPackagePage.decisionPackages, "Decision Packages");
+		dpp.jsClickOn(DecisionPackagePage.newDecisionPackage, "newDecisionPackage");
+		// Creation of Decision package
+		dpp.verifyUserCanCreate_DecisionPackage(DecisionPackagePage.SubmissionTypeOption_Contract, "Contract");
+//		dpp.verifyDueDate("Due Date On Emergency");
+		// Validation of required fields after decision package creation 
+		dpp.verifyMandatoryFieldsOn_Contract_DecisionPackage("E&E-HHS-Contract");
+		dpp.selectEmergencyRequestInthe_DecisionPackage();
+//		dpp.verifyDueDate("Due Date when No Emergency");
 	}
 }
